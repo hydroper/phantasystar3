3a9de448515053cefb8ed111c6e2e5283bcbf27c
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-06-21 19:40:02 -0300

    _

---
+++ client/src/ui/inventory/ps3_selectable_tech_button.gd
@@ -0,0 +1,20 @@
+class_name PS3SelectableTechButton
+extends Control
+
+var tech: PS3TechType
+
+func display_tech(tech: PS3TechType) -> void:
+    self.tech = tech
+    $button/content/container/right/label.text = self.tech.name
+
+static func get_pressed_from_list(list: Variant) -> PS3SelectableTechButton:
+    if list is Array:
+        var f = list.filter(func(a): return a.get_node("button").button_pressed)
+        return null if len(f) == 0 else f[0]
+    return PS3SelectableTechButton.get_pressed_from_list(list.get_children()) if list is Node else null
+
+static func get_focused_from_list(list: Variant) -> PS3SelectableTechButton:
+    if list is Array:
+        var f = list.filter(func(a): return a.get_node("button").has_focus())
+        return null if len(f) == 0 else f[0]
+    return PS3SelectableTechButton.get_focused_from_list(list.get_children()) if list is Node else null
