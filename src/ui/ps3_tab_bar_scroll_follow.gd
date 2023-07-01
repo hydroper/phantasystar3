8e572aad3e4c3d776b751bfaa9c0de9e1bef094d
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-07-01 17:32:50 -0300

    _

--- src/ui/ps3_tab_bar_scroll_follow.gd
+++ src/ui/ps3_tab_bar_scroll_follow.gd
@@ -4,5 +4,10 @@ class_name PS3TabBarScrollFollow
 
 static func apply(tab_bar: PS3TabBar, container: ScrollContainer) -> void:
     tab_bar.tab_changed.connect(func(_tab):
-        var btn = tab_bar.current_tab_button
-        container.scroll_horizontal = int(btn.global_position.x - container.global_position.x))
+        var tab_bar_start_x = tab_bar.global_position.x
+        var tab_bar_end_x = tab_bar_start_x + tab_bar.size.x
+        var btn := tab_bar.current_tab_button
+        var btn_x := btn.global_position.x
+        if (btn_x + btn.size.x) < tab_bar_start_x or btn_x > tab_bar_end_x:
+            var btn_rel_x := btn.global_position.x - container.global_position.x
+            container.scroll_horizontal = int(btn_rel_x))
