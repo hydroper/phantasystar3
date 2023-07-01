67efc2a869707bd7ebe46d7f51cc8355576885ee
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-07-01 15:20:17 -0300

    _

--- src/screens/game/menu/items/game_sc_items_menu.gd
+++ src/screens/game/menu/items/game_sc_items_menu.gd
@@ -8,6 +8,7 @@ func open(data: Variant) -> void:
     $item_selector.popup()
     $context/outer.visible = false
     $report/outer.visible = false
+    $target_char/outer.visible = false
     self._update_items()
 
 # Closes any sublayer and the current layer itself.
@@ -16,6 +17,7 @@ func close(_data: Variant) -> void:
     $item_details.collapse()
     $context/context.collapse()
     $report/report.collapse()
+    $target_char/target_char.collapse()
 
 # If there is any sublayer, closes only it; if none,
 # closes the current layer.
@@ -24,6 +26,8 @@ func close_sublayer(_data: Variant) -> void:
         $context/context.collapse("close_context")
     elif $report/report.is_open:
         $report/report.collapse("close_report")
+    elif $target_char/target_char.is_open:
+        $target_char/target_char.collapse("close_target_char")
     else:
         $item_selector.collapse("close_current")
         $item_details.collapse()
@@ -35,7 +39,6 @@ var _sublayer: UISublayer = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
