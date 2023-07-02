261ed07eec8b319207bbc327a073af845c270df1
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-07-02 07:54:37 -0300

    _

--- src/screens/intro/intro_screen.gd
+++ src/screens/intro/intro_screen.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
+@onready
+var _animator: AnimationPlayer = $root/animator
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    pass # Replace with function body.
+func _ready() -> void:
+    self._animator.animation_finished.connect(func(_anim_name):
+        pass)
+    $root/skip_btn.pressed.connect(func():
+        self._skip_part())
 
+func _input(event):
+    if event.is_action_released("ui_cancel"):
+        self._skip_part()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta: float) -> void:
     pass
+
+func _skip_part() -> void:
+    var pos = self._animator.current_animation_position
+    for ahead_pos in [0.4, 2.6, 3, 4.6]:
+        if pos < ahead_pos:
+            self._animator.advance(ahead_pos)
+            break
