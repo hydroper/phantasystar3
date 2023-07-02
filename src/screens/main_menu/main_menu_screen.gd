871cf7f9949494fac57fbb0772b87e552a26cda0
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-07-02 08:29:31 -0300

    _

--- src/screens/main_menu/main_menu_screen.gd
+++ src/screens/main_menu/main_menu_screen.gd
@@ -6,7 +6,19 @@ func _ready() -> void:
     $root/start_layer/animator.animation_finished.connect(func(anim_name):
         if anim_name == "default":
             $root/start_layer/animator.play("looping")
-            self._waiting_start = true)
+            self._waiting_start = true
+        elif anim_name == "start_pressed":
+            pass)
+    $root/start_layer/start_click_btn.pressed.connect(self._on_start_press)
+
+func _input(event):
+    if event.is_action_released("ui_accept"):
+        self._on_start_press()
 
 func _process(_delta: float) -> void:
+    pass
+
+func _on_start_press() -> void:
+    if not self._waiting_start:
+        return
     pass
